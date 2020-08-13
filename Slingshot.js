b8f4b4d28ff73a06564944a03ba04d33235cca16
tanyachandwani <ruchitachandwani@gmail.com>
2020-08-13 18:44:10 +0530

    Created catapult

--- Slingshot.js
+++ Slingshot.js
@@ -6,6 +6,10 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
+        this.Sling1=loadImage("sprites/sling1.png");
+        this.Sling2=loadImage("sprites/sling2.png");
+        this.Sling3=loadImage("sprites/sling3.png");
+
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
@@ -16,12 +20,26 @@ class SlingShot{
     }
 
     display(){
+        image(this.Sling1,200,20)
+        image(this.Sling2,170,20)
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
-        }
+            if (pointA.x<220){
+                strokeWeight(7);
+                stroke(48,22,8);
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x+30, pointB.y);
+                image(this.Sling3,pointA.x-30,pointA.y-10,15,30);
+             
+            }else{
+                strokeWeight(4);
+            stroke(48,22,8);
+            line(pointA.x+20, pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x+20, pointA.y, pointB.x+30, pointB.y);
+            image(this.Sling3,pointA.x+20,pointA.y-10,15,30); 
+            }
+            }
     }
     
 }
